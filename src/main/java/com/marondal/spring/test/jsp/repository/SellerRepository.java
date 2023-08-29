7aaa77c7ad4bd257ee47fd5e510416da4373e502
qkrwhdgh24 <jh.corder.ssam@gmail.com>
2023-08-29 17:51:50 +0900

    [김인규]  - jsp 기본 문제 풀이

---
+++ src/main/java/com/marondal/spring/test/jsp/repository/SellerRepository.java
@@ -0,0 +1,21 @@
+package com.marondal.spring.test.jsp.repository;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.marondal.spring.test.jsp.domain.Seller;
+
+@Repository
+public interface SellerRepository {
+	
+	public int insertSeller(
+			@Param("nickname") String nickname
+			, @Param("profileImage") String profileImage
+			, @Param("temperature") double temperature);
+	
+	
+	public Seller selectLastSeller();
+	
+	public Seller selectSeller(@Param("id") int id);
+
+}
---
+++ src/main/java/com/marondal/spring/test/jsp/service/SellerService.java
@@ -0,0 +1,36 @@
+package com.marondal.spring.test.jsp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.spring.test.jsp.domain.Seller;
+import com.marondal.spring.test.jsp.repository.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	public int addSeller(String nickname, String profileImage, double temperature) {
+		int count = sellerRepository.insertSeller(nickname, profileImage, temperature);
+		
+		return count;
+	}
+	
+	
+	public Seller getLastSeller() {
+		Seller seller = sellerRepository.selectLastSeller();
+		
+		return seller;
+	}
+	
+	public Seller getSeller(int id) {
+		
+		Seller seller = sellerRepository.selectSeller(id);
+		
+		return seller;
+		
+	}
+
+}
